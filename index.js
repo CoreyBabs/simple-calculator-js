5003a681f5bc298c623d2beaf546e5e19a59d31f
CoreyBabs <coreymbabcock@gmail.com>
2023-07-08 12:10:18 -0600

    Add basic operation handling

--- index.js
+++ index.js
@@ -13,3 +13,21 @@ function multiply(a, b) {
 function divide(a, b) {
 	return +(a / b).toFixed(2);
 }
+
+function operate(a, b, op) {
+	switch(op) {
+		case "+": return add(a,b);
+		case "-": return subtract(a,b);
+		case "*": return multiply(a,b);
+		case "/": {
+			if (b === 0) return "This calculator is not smart enough to divide by 0";
+			return divide(a, b);
+		}
+	}
+}
+
+let firstValue = null;
+let secondValue = null;
+let operator = null;
+
+
