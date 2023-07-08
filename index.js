4f2a9613185d7b43924b26f6e8f39be13e6b3a08
CoreyBabs <coreymbabcock@gmail.com>
2023-07-08 15:21:30 -0600

    add button functionality except for equals

--- index.js
+++ index.js
@@ -26,8 +26,53 @@ function operate(a, b, op) {
 	}
 }
 
+function addToDisplay(txt) {
+	let display = document.querySelector("#display");
+	display.textContent += txt;
+}
+
+function removeFromDisplay() {
+	let display = document.querySelector("#display");
+	let idx = -1;
+
+	// if a space is detected, that means we need to delete an op which has whitespace
+	if (display.textContent.slice(-1) === " ") lastDisplayMode = "op";
+
+	// operators are surrouned by a space so include that in the delete.
+	if (lastDisplayMode === "op") {
+		idx = -3;
+		lastDisplayMode = "num";
+	}
+
+	display.textContent = display.textContent.slice(0, idx);
+}
+
+function clearDisplay() {
+	let display = document.querySelector("#display");
+	display.textContent = "";
+}
+
 let firstValue = null;
 let secondValue = null;
 let operator = null;
+let lastDisplayMode = "None";
 
+let numbers = document.querySelectorAll("#numbers");
+numbers.forEach(n => n.addEventListener("click", (e) => { 
+	addToDisplay(e.target.textContent);
+	lastDisplayMode = "num";
+}));
 
+let ops = document.querySelectorAll(".op");
+ops.forEach(o => o.addEventListener("click", (e) => {
+	if (lastDisplayMode !== "op") {
+		addToDisplay(` ${e.target.textContent} `);
+		lastDisplayMode = "op";
+	}
+}));
+
+let clear = document.querySelector("#clear");
+clear.addEventListener("click", clearDisplay);
+
+let del = document.querySelector("#delete");
+del.addEventListener("click", removeFromDisplay);
